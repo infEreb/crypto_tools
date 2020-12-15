a28a99f390487ce08cb76838d4c4012aadaea043
Egor <savushkin.egor.00027@gmail.com>
2020-12-15 16:12:29 +0300

    Fixed bugs lower/upper case turned into upper/lower (abczABCZ turned into bcdACDa). Added '-c' flag. Fixed some code.

--- caesar_chip.c
+++ caesar_chip.c
@@ -1,15 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 #define HELP_MESS "Ceaser_Chiper 0.1 created by ie.\n\nOptions:\n\t-h - show this message.\n\n\t-f=<path/to/file> - input encrypt/decrypt data file.\n\n\t-c=<shift_count> - count of shifting from 'a' (3 as default)."
 
-#define MAX_LENGHT 1000
+#define MAX_LENGHT 1000 // lenght of encrypt/decrypt message
 #define DEFAULT_SHIFTING 3
 
-int *de_file(const char *file_name, int shift) // decrypt/encrypt data function
+
+#define HELP_FLAG "-h"
+#define FILE_FLAG "-f="
+#define SHIFT_FLAG "-c="
+
+int help_message(void)
 {
-    const int end_of_letters = 'z' - shift;
+	printf("Execute this program with '-h' option.\n");
+	return -1;
+}
+
+int *de_file(const char *file_name, int shift) // decrypt/encrypt data
+{
+    const int downReg_end = 'z' - shift;
     const int upReg_end = 'Z' - shift;
     
     FILE *data_file = fopen(file_name, "r");
@@ -22,6 +34,16 @@ int *de_file(const char *file_name, int shift) // decrypt/encrypt data function
     
     int *data = (int *)malloc(MAX_LENGHT);
     int *data_iter = data;
+
+	if(shift <= 0 || shift >= 26)
+	{
+		while(fread(data_iter, 1, 1, iter_file))
+		{
+			data_iter++;
+		}
+		fclose(data_file);
+		return data;
+	}
     
     while((*data_iter = fgetc(iter_file)) != EOF)
     {
@@ -31,35 +53,50 @@ int *de_file(const char *file_name, int shift) // decrypt/encrypt data function
     while(*data_iter != EOF)
     {
         //printf("%c - %d\n", *data_iter, *data_iter);
-        if (*data_iter == '\n' || *data_iter == '\t' || *data_iter == ' ')
+		
+        if (!isalpha(*data_iter))
         {
             data_iter++;
             continue;
         }
         
-        int a[3] = {'a', 'b', 'c'};
-        *a += shift;
-        
-        if (*data_iter > end_of_letters)
+        if (*data_iter >= 'a' && *data_iter > downReg_end)
         {
-            
-            *data_iter = 'A' + shift - ('z'-(*data_iter)+1);
-            data_iter++;
-            //printf(">");
-            continue;
+        	*data_iter = 'a' + *data_iter - downReg_end - 1;
+        	data_iter++;
+			continue;
         }
-        if(*data_iter > upReg_end && *data_iter <= 'Z')
-        {
-            *data_iter += shift + 3;
-        }
-        
-        *data_iter = *data_iter + shift;
-        data_iter++;
-            
+		else if (*data_iter <= 'Z' && *data_iter > upReg_end)
+		{
+        	*data_iter = 'A' + *data_iter - upReg_end - 1;
+        	data_iter++;
+			continue;
+		}
+		*data_iter = *data_iter + shift;
+		data_iter++;
     }
     *data_iter = '\0';
     
     return data;
+}
+
+char flag_definer(char *flag)
+{
+		if(*flag == '-')
+		{
+			if (!strcmp(flag, HELP_FLAG))
+				return 'h';
+			if (*(flag+2) == '=')
+			{
+				if (*(flag+1) == 'f')
+					return 'f';
+				else if (*(flag+1) == 'c')
+					return 'c';
+				else
+					exit(help_message());
+			}
+		}
+		return '0';
 }
 
 int main(int argc, char **argv)
@@ -68,19 +105,13 @@ int main(int argc, char **argv)
 	
 	switch(argc) {
 		case 1:
-			printf("Enter '%s -h' for help\n", *argv);
-			return -1;
+			return help_message();
+
 		case 2:
-			if (!strcmp(*(argv+1), "-h"))
-            {
-				printf("%s\n", HELP_MESS);
-                return 0;
-            }
-            char *f = "-f=";
             for(int i = 0; i < 3; i++)
             {
-                if ((*(*(argv+1)+i) == *(f+i))) continue;
-                else printf("Enter '%s -h' for help\n", *argv); return -1;
+                if ((*(*(argv+1)+i) == *(FILE_FLAG+i))) continue;
+                else return help_message();
             }
             
             int *de_data = de_file(*(argv+1)+3, DEFAULT_SHIFTING);
@@ -88,9 +119,71 @@ int main(int argc, char **argv)
             free(de_data);
 
 			break;
+
+		case 3:
+			;
+			char fg;
+			int shift = DEFAULT_SHIFTING;
+			
+			for(int i = 1; i <= 2; i++)
+			{
+				fg = flag_definer(*(argv+i));
+				if (fg == 'c') shift = atoi((*(argv+i)+3));
+			}
+			for(int i = 1; i <= 2; i++)
+			{
+				fg = flag_definer(*(argv+i));
+				switch(fg)
+				{
+					case 'h':
+						help_message();
+						break;
+
+					case 'f':
+						;
+						int *de_data = de_file(*(argv+i)+3, shift);
+						printf("%ls\n", de_data);
+						break;
+
+					default:
+						break;
+				}
+			}
+			break;
+		
+		case 4:
+			;
+			fg = ' ';
+			shift = DEFAULT_SHIFTING;
+			
+			for(int i = 1; i <= 3; i++)
+			{
+				fg = flag_definer(*(argv+i));
+				if (fg == 'c') shift = atoi((*(argv+i)+3));
+			}
+			for(int i = 1; i <= 3; i++)
+			{
+				fg = flag_definer(*(argv+i));
+				switch(fg)
+				{
+					case 'h':
+						help_message();
+						break;
+
+					case 'f':
+						;
+						int *de_data = de_file(*(argv+i)+3, shift);
+						printf("%ls\n", de_data);
+						break;
+
+					default:
+						break;
+				}
+			}
+			break;
+
 		default :
-			printf("Enter '%s -h' for help\n", *argv);
-			return -1;
+			return help_message();
                 
     }
 
